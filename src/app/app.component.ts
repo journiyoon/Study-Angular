8363619817d507d33c572c6527eabc1edc178e50
journiyoon <journiyoon@gmail.com>
2023-10-18 06:59:45 +0900

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { WishItem } from 'src/shared/model/wishItem';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  items = [
+    new WishItem('To Learn Angular'),
+    new WishItem('To Change Job'),
+    new WishItem('To be the Rich'),
+  ];
   title = 'wishlist';
 }
